b79e772ac218ce3b0ee9a443d396d2ff65d4857b
Christopher LaChance <8072374+CTLaChance@users.noreply.github.com>
2020-04-16 14:05:24 -0500

    Slight refactor.
    Added a basic whitelist.

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
-require('@tensorflow/tfjs-node');
 let toxicity = require('@tensorflow-models/toxicity');
 let Discord = require('discord.js');
 let client = new Discord.Client();
+
+const whitelist = ["dumb", "stupid"];
 
 client.login(`${process.env.CONNECTION_TOKEN}`)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-})
+});
 
 client.on('message', async msg => {
     console.log(`${msg.author.username} said: ${msg.content}`)
@@ -16,36 +17,26 @@ client.on('message', async msg => {
     // Prevent the bot from responding to itself.
     if(msg.author.bot) return;
 
-    const threshold = 0.9;
-    toxicity.load(threshold).then(model => {
+    // Checks the string for a whitelisted word.
+    if(whitelist.some(substring => msg.content.toLowerCase().includes(substring))) {
+        console.log("This message contains a whitelisted word.");
+        return;
+    }
+
+    toxicity.load(0.95).then(model => {
         model.classify(msg.content).then(predictions => {
-          // Predictions[6] is the overall toxicity for the message.
-          if (predictions[6].results[0].match) {
-            console.log("This message is toxic.");
-            
-            // Play the toxic soundbyte.
-            if(msg.member.voiceChannel)
-            {
-                msg.member.voiceChannel.join().then( connection => {
-                    let dispatcher = connection.playFile("./toxic.mp3", {volume: 0.2});
-
-                    // Dispatcher 'end' event is not firing. Temp workaround.
-                    setTimeout(function(){
-                        dispatcher.end();
-                        msg.member.voiceChannel.leave();
-                    }, 2000);
-                });
+            // Predictions[6] is the overall toxicity for the message.
+            if (predictions[6].results[0].match) {
+                console.log("This message is toxic.");
+                
+                msg.react("680243320852709473")
+                    .then(() => msg.react("🇹"))
+                    .then(() => msg.react("🇴"))
+                    .then(() => msg.react("🇽"))
+                    .then(() => msg.react("🇮"))
+                    .then(() => msg.react("🇨"))
+                    .then(() => msg.react("☣️"));
             }
-
-            msg
-              .react("680243320852709473")
-              .then(() => msg.react("🇹"))
-              .then(() => msg.react("🇴"))
-              .then(() => msg.react("🇽"))
-              .then(() => msg.react("🇮"))
-              .then(() => msg.react("🇨"))
-              .then(() => msg.react("☣️"));
-          }
         });
     });
 });
